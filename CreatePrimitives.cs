21c3df5beedc15401bee3817e4b0bb07dc3c68e2
denisGladkov349 <user-007gladkov@yandex.ru>
2025-09-02 13:26:35 +0700

    CreatePrimitives.cs

---
+++ CreatePrimitives.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class CreatePrimitives : MonoBehaviour
+{
+    void Start()
+    {
+        // Создание фигуры, напоминающей Стива из Minecraft, из шести кубов
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0, 0, 0);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(-0.5f, 1, 0);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0.5f, 1, 0);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0.5f, 2, 0);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(-0.5f, 2, 0);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0, 3, 0);
+    }
+}
