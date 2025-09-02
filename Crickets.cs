c33898fb2bd35076ab34f6eb3b3911006aaac56b
denisGladkov349 <user-007gladkov@yandex.ru>
2025-09-02 13:22:43 +0700

    Crickets.cs

---
+++ Crickets.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Crickets : MonoBehaviour
+{
+    // Выводим слово "crickets" в консоль каждый кадр
+    void Update()
+    {
+        Debug.Log("crickets");
+    }
+}
